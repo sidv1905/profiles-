52f7dc3ae45b2aaf01937f73991035661706434d
sidv1905 <sidvarangaonkar1905@gmail.com>
2020-07-16 13:28:12 +0530

    Viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+from rest_framework import viewsets
 
-# Create your views here.
+class HelloAPIView(APIView):
+    serializer_class=serializers.HelloSerializer
+    def get(self,request,format=None):
+        api_list=['I am the best',' I am scared a bit', ' I should be scared']
+        return Response({'message':'hello','an_apiview':api_list})
+    def post(self,request):
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_404_BAD_REQUEST)
+    def put(self,request,pk=None):
+        '''Replace the object'''
+        return Response({'method':'PUT'})
+    def patch(self,request,pk=None):
+        '''update specified only'''
+        return Response({'method':'PATCH'})
+    def delete(self,request,pk=None):
+        '''Delete Specified object'''
+        return Response({'method':'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class=serializers.HelloSerializer
+    def list(self,request):
+        api_list=['I am the best',' I am scared a bit', ' I should be scared']
+        return Response({'message':'hello me viewset hu','an_apiviewset':api_list})
+    def create(self,request):
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello{name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,
+            status=status.HTTP_404_BAD_REQUEST)
+    def retrieve(self,request,pk=None):
+        return Response({"http-method":"GET"})
+    def update(self,request,pk=None):
+        return Response({"http-method":"PUT"})
+    def partial_update(self,request,pk=None):
+        return Response({"http-mehtod":"PATCH"})
+    def destroy(self,request,pk=None):
+        return Response({"http-method":"DELETE"})
