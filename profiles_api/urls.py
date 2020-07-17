deda71e9c40666d0c9ebc8e1e5adde4d60a1059d
sidv1905 <sidvarangaonkar1905@gmail.com>
2020-07-17 11:05:25 +0530

    99

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,7 +6,7 @@ router=DefaultRouter()
 
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile',views.UserProfileViewSet,base_name='profile')
-
+router.register('feed',views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/',views.HelloAPIView.as_view()),
